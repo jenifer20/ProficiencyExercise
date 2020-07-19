a21fa125573be7d0356ddc0313d7a9acde3fa846
Jeni <jenifer.s@borngroup.com>
2020-07-19 19:50:59 +0530

    Initial Commit

---
+++ ProficiencyExercise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProficiencyExercise
+//
+//  Created by Jeni on 19/07/20.
+//  Copyright © 2020 Jeni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
